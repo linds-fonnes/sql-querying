a0f73d49cbc48562c3f5fc3a1f7f90e629bf1858
Lindsey <linds.fonnes@gmail.com>
2021-03-30 10:36:57 -0600

    first commit

---
+++ queries_playstore.sql
@@ -0,0 +1,20 @@
+-- Comments in SQL Start with dash-dash --
+ --1. SELECT * FROM analytics WHERE id = 1880;
+ --2. SELECT id, app_name FROM analytics WHERE last_updated = '2018-08-01';
+ --3. SELECT category, COUNT(category) FROM analytics GROUP BY category;
+ --4.SELECT app_name, reviews FROM analytics ORDER by reviews DESC LIMIT 5;
+ --5.SELECT app_name, reviews, rating FROM analytics WHERE rating >= 4.8 ORDER by reviews DESC LIMIT 1;
+ --6. SELECT AVG(rating), category FROM analytics GROUP BY category ORDER BY AVG(rating) DESC;
+ --7. SELECT app_name, price, rating FROM analytics WHERE rating < 3 ORDER BY price DESC LIMIT 1;
+--8.SELECT app_name,rating,min_installs FROM analytics WHERE min_installs < 50 AND rating > 0 ORDER BY rating DESC;
+--9.SELECT app_name FROM analytics WHERE rating < 3 AND reviews > 10000;
+--10.SELECT app_name FROM analytics WHERE price < 1.00 AND price > 0.10 LIMIT 10;
+--11. SELECT * FROM analytics ORDER BY last_updated LIMIT 1;
+--12.  SELECT app_name, price FROM analytics ORDER BY price DESC LIMIT 1;
+--13.SELECT SUM(reviews) FROM analytics;
+--14. SELECT category FROM analytics GROUP BY category HAVING COUNT(*) > 300;
+--15. SELECT app_name, reviews, min_installs, min_installs/reviews AS proportion FROM analytics WHERE min_installs >= 100000 ORDER BY proportion DESC LIMIT 1;
+--FS1. SELECT app_name, rating, category FROM analytics WHERE (rating,category) in ( SELECT MAX(rating), category FROM analytics WHERE min_installs > 50000 GROUP BY category) ORDER BY category;
+--FS2. SELECT app_name FROM analytics WHERE app_name ILIKE '%facebook%';
+--FS3. SELECT app_name FROM analytics WHERE array_length(genres,1) > 1;
+--FS4.SELECT app_name FROM analytics WHERE genres @> '{"Education"}';
